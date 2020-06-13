7b706556f455d1e9f93f13f3d38dbccad5a9aee3
Marcos Pietrucci <marcospietrucci@gmail.com>
2020-06-13 19:55:11 -0300

    Adição inicial do programa

---
+++ programa/programa.ino
@@ -0,0 +1,22 @@
+//Instalar a biblioteca de suporte a ESP32
+
+#define motor1 5;
+#define motor2 6;
+#define sensor_direita 7;
+#define sensor_esquerda 8;
+
+/*---- Pinos da sensor Array (mais pesquisa necessária) ----//
+*
+*
+*
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
